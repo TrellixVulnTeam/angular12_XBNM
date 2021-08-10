869e6313540106cd320a256545b64acc3c108be2
mahesh perla <maheshp212@gmail.com>
2021-08-10 09:54:19 +0530

    routes

--- drys/src/app/app-routing.module.ts
+++ drys/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { VariablesComponent } from './variables/variables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'intro', component: IntroComponent},
+  {path:'varrrrrrrr', component: VariablesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- drys/src/app/app.module.ts
+++ drys/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IntroComponent } from './intro/intro.component';
+import { VariablesComponent } from './variables/variables.component';
 
 @NgModule({
   declarations: [ // components, directives, pipes
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IntroComponent,
+    VariablesComponent
   ],
   imports: [ //modules
     BrowserModule,
